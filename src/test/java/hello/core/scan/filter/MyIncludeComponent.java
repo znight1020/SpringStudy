a93da15be00199e5bc6d86ba49a23d56eed20688
현수 <znight1020@naver.com>
2023-03-23 21:41:25 +0900

    컴포넌트 스캔의 필터 적용

--- src/test/java/hello/core/scan/filter/MyIncludeComponent.java
+++ src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -1,4 +1,14 @@
 package hello.core.scan.filter;
 
+import java.lang.annotation.*;
+
+/**
+ * Annotation을 직접 만들어 봅시다.
+ * 해당 Annotation(MyIncludeComponent)가 붙은 클래스는 컴포넌트 스캔의 추가 대상이 될 것
+ * */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface MyIncludeComponent {
+
 }
