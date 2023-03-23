206d5d4332d06ddf0fdc55e7f1d30bc4ff54c445
현수 <znight1020@naver.com>
2023-03-23 21:17:13 +0900

    컴포넌트 스캔과 탐색 위치, 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,4 +1,15 @@
 package hello.core;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+) // Configuration Annotation이 붙은 클래스는 빈에 등록하지 않겠다.
 public class AutoAppConfig {
+
 }
