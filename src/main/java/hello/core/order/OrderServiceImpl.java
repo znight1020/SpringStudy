926b5f6d7a3fc22a90d07f8809843bbc006519e1
현수 <znight1020@naver.com>
2023-03-24 08:46:56 +0900

    조회 빈이 2개 이상일 때 해결하는 3가지 방법(@Autowired 파라미터 명, @Qualifier, @Primary)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,13 +4,19 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final이 붙으면 필수값이 되는데 이것들을 보고 해당하는 생성자를 만들어준다. 굿굿
+//@RequiredArgsConstructor // final이 붙으면 필수값이 되는데 이것들을 보고 해당하는 생성자를 만들어준다. 굿굿
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
